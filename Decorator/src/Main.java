4e08276a09cf8835258eb488259f9a11ae1e403c
jbwolfe <jbwolfe@crimson.ua.edu>
2015-07-15 13:48:25 -0500

    Adding decorator pattern

--- Decorator/src/Main.java
+++ Decorator/src/Main.java
@@ -1,4 +1,13 @@
 
 public class Main {
-
+	public static void main(String[] args) {
+		Pizza basicPizza = new TomatoSauce(new Mozzarella(new PlainPizza()));
+		Pizza noCheesePizza = new TomatoSauce(new PlainPizza());
+		
+		System.out.println("Ingredients: " + basicPizza.getDescription());
+		System.out.println("Cost: " + basicPizza.getCost());
+		System.out.println();
+		System.out.println("Ingredients: " + noCheesePizza.getDescription());
+		System.out.println("Cost: " + noCheesePizza.getCost());
+	}
 }
