0746c6d7687291f6a943ff005b5d1092e353cf55
jbwolfe <jbwolfe@crimson.ua.edu>
2015-07-15 13:30:01 -0500

    Adding prototype pattern

--- Prototype/src/Main.java
+++ Prototype/src/Main.java
@@ -1,4 +1,15 @@
 
 public class Main {
-
+	public static void main(String[] args) {
+		CloneFactory animalMaker = new CloneFactory();
+		
+		Sheep sally = new Sheep();
+		
+		Sheep clonedSheep = (Sheep)animalMaker.getClone(sally);
+		
+		System.out.println(sally);
+		System.out.println(clonedSheep);
+		System.out.println("Sally's location: " + System.identityHashCode(sally));
+		System.out.println("Cloned sheep's location: " + System.identityHashCode(clonedSheep));
+	}
 }
